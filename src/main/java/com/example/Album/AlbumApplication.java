d3c7faf7d6df45979a0b0a0493dd1ed3a4d6cae5
DESKTOP-BC558MM\Abi <abhi91420@gmail.com>
2022-05-16 15:37:45 -0400

    Initial commit

---
+++ src/main/java/com/example/Album/AlbumApplication.java
@@ -0,0 +1,15 @@
+package com.example.Album;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class AlbumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/Album/service/AlbumService.java
@@ -0,0 +1,43 @@
+package com.example.Album.service;
+
+import com.example.Album.model.Album;
+import com.example.Album.repository.AlbumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AlbumService
+{
+    @Autowired
+    private AlbumRepository albumRepository;
+    public Album getAlbum()
+    {
+        return albumRepository.getAlbum();
+    }
+
+    public Album saveAlbum(Album album)
+    {
+        return albumRepository.saveAlbum(album);
+    }
+
+    public List<Album> getallAlbum() {
+        return albumRepository.getallAlbum();
+    }
+
+    public Album getAlbumbyID(int albumID)
+    {
+        return albumRepository.getAlbumbyID(albumID);
+    }
+
+    public Album updatelbum(int albumID, Album album)
+    {
+        return albumRepository.updateAlbum(albumID, album);
+    }
+
+    public Album deleteAlbum(int albumID)
+    {
+        return albumRepository.deleteAlbum(albumID);
+    }
+}
